63eaba74e19f29666781eac7c778d9fb0e570cdc
nnvedward <nnvedward@gmail.com>
2022-04-21 15:20:39 +0100

    Setup transfer function

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ async function createUser(username, password) {
       await t.rollback();
       return {
         success: false,
-        error: 'there was error',
+        error: 'An error occured',
       };
     }
   }
@@ -159,9 +159,80 @@ async function withdraw(account_id, amount) {
       await t.rollback();
       return {
         success: false,
-        error: 'there was error',
+        error: 'An error occured',
       };
     }
   }
 
 //   withdraw(3, 2000).then(console.log).catch(console.log)
+
+/**
+ * @param {number} sender_id account_id of the sender
+ * @param {number} recipient_id account_id of the recipient
+ * @param {number} amount amount to deposit
+ */
+
+async function transfer(sender_id, recipient_id, amount) {
+    const schema = joi.object({
+        sender_id: joi.number().required(),
+        recipient_id: joi.number().required(),
+        amount: joi.number().min(1).required()
+    })
+
+    const validation = schema.validate({ sender_id, recipient_id, amount })
+    if (validation.error) {
+        return {
+            success: false,
+            error: validation.error.details[0].message
+        }
+    }
+
+    const t = await models.sequelize.transaction()
+
+    try {
+        const reference = v4()
+        const purpose = 'transfer'
+
+        const transferResult = await Promise.all([
+            debitAccount({
+                amount,
+                account_id: sender_id,
+                purpose,
+                reference,
+                metadata: {
+                    recipient_id
+                },
+                t
+            }),
+            creditAccount({
+                amount,
+                account_id: recipient_id,
+                purpose,
+                reference,
+                metadata: {
+                    sender_id
+                }
+            })
+        ])
+
+        const failedTxns = transferResult.filter((result) => !result.success)
+        if (failedTxns.length) {
+            await t.rollback()
+            return transferResult
+        }
+
+        await t.commit()
+        return {
+            success: true,
+            message: 'Transfer successful'
+        }
+    } catch (error) {
+        await t.rollback()
+        return {
+            success: false,
+            error: 'Internal server error'
+        }
+    }
+}
+
+transfer(2, 3, 60000).then(console.log).catch(console.log)
